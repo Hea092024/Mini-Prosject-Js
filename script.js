f2c572e877fca9b3465d7e2435b9b503362c1355
Hea092024 <Hea092024@gmail.com>
2024-12-03 09:58:16 +0100

    .

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 const inputNum = document.querySelector("#input-num");
 const circleContainer = document.querySelector("#circle-cotainer");
+// circleContainer.remove();
+
+const makeCircles = () => {
+    console.log("knappen virker")
+}
