4ba92785107e3f7afda784cfabef844a86d82c87
Hea092024 <Hea092024@gmail.com>
2024-12-03 10:02:06 +0100

    updated js with comments

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+//Getting the HTML elements from index.html
 const inputNum = document.querySelector("#input-num");
 const circleContainer = document.querySelector("#circle-cotainer");
 // circleContainer.remove();
 
+//function to make the cicrles.
 const makeCircles = () => {
-    console.log("knappen virker")
+    // console.log("knappen virker")
+    
 }
